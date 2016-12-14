118e97619f3e1eb7781be93fd94c78f4ca1373ed
imosebro <imosebro@u.rochester.edu>
2016-12-14 18:14:36 -0500

    Initial Commit
    Set up the repository with the start of the work

---
+++ index.php
@@ -0,0 +1,51 @@
+    
+<?php include "inc/header.inc"; ?>
+
+  <?php include "inc/nav.inc" ?>
+
+
+    <!-- Top Section -->
+    <section id="first">
+
+        <h1 class="jumboHeader">Create Incredible Websites</h1>
+
+        <div class="bottomText">
+
+            <p>Isaac Mosebrook Websites are the finest
+                pieces of web work available today</p>
+
+            <a class="button itemRight" href="">Get Yours Now</a>
+
+        </div>
+
+    </section>
+
+    <!-- Second Section -->
+    <section id="second">
+
+        <h1 class="jumboHeader">Beautiful Aesthetics</h1>
+
+        <div class="bottomText">
+            <p>Sleek and Modern designs with
+                user friendly Interfaces</p>
+
+            <a class="button itemRight" href="">See Examples Now</a>
+        </div>
+
+    </section>
+
+    <!-- Third Section -->
+    <section id="third">
+              
+        <h1 class="jumboHeader">Wide Range of Applications</h1>
+
+        <div class="bottomText">
+            <p>From building a custom wordpress site
+                to fixing up your outdated work</p>
+
+            <a class="button itemRight" href="">See Examples Now</a>
+        </div>
+
+    </section>
+
+    <?php include "inc/footer.inc"; ?>
